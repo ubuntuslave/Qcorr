2334759c8d27f1eae1b9545437a3ad2d9c75be6b
Carlos Jaramillo <ubuntuslave@gmail.com>
2009-12-07 01:02:08 -0500

    Final Commit for Report and Presentation Dec 7, 2009

--- src/controlswindow.cpp
+++ src/controlswindow.cpp
@@ -1,8 +1,8 @@
 #include "controlswindow.h"
 #include "qcorr.h"
 
-ControlsWindow::ControlsWindow(Qcorr *parentWindow, QWidget *parent)
-    : QDialog(parent)
+ControlsWindow::ControlsWindow(Qcorr *parentWindow, QWidget *parent) :
+   QDialog(parent)
 {
    ui.setupUi(this);
 
@@ -12,58 +12,75 @@ ControlsWindow::ControlsWindow(Qcorr *parentWindow, QWidget *parent)
 
    setEnableSpinBoxes(false);
 
-	createActions();
+   createActions();
 
-	// Default values:
-	m_nScanInterval = 10;
-	m_nTemplateSize = 32;
+   // Default values:
+   m_nScanInterval = 10;
+   m_nTemplateSize = 32;
 
 }
 
 ControlsWindow::~ControlsWindow()
 {
    emit this->close();
-   delete this;   // Destroys itself
+   delete this; // Destroys itself
 }
 
-
-void ControlsWindow::createActions()
+void
+ControlsWindow::createActions()
 {
    connect(ui.close_pushButton, SIGNAL(clicked()), this, SLOT(close()));
 
-   connect(ui.scanInterval_spinBox, SIGNAL(valueChanged(int)),
-           this, SLOT(setScanInterval(int)));
+   connect(ui.scanInterval_spinBox, SIGNAL(valueChanged(int)), this,
+         SLOT(setScanInterval(int)));
 
-   connect(ui.templateSize_spinBox, SIGNAL(valueChanged(int)),
-           this, SLOT(setTemplateSize(int)));
-
+   connect(ui.templateSize_spinBox, SIGNAL(valueChanged(int)), this,
+         SLOT(setTemplateSize(int)));
 
 }
 
-void ControlsWindow::setEnableSpinBoxes(bool bEnable)
+void
+ControlsWindow::setEnableSpinBoxes(bool bEnable)
 {
    // Setup default values for spinBoxes
 
    ui.scanInterval_spinBox->setEnabled(bEnable);
    ui.templateSize_spinBox->setEnabled(bEnable);
 
-   if(bEnable)
+   if (bEnable)
       {
-      // Sets maximum value of scanInterval_spinBox according to the right image's width
-      ui.scanInterval_spinBox->setMaximum(m_parentWindow->m_rightImage->width());
-      // Sets maximum value of templateSize_spinBox according to the right image's width
-      ui.templateSize_spinBox->setMaximum(m_parentWindow->m_rightImage->width());
+         // Sets maximum value of scanInterval_spinBox according to the right image's width
+         ui.scanInterval_spinBox->setMaximum(
+               m_parentWindow->m_rightImage->width());
+         // Sets maximum value of templateSize_spinBox according to the right image's width
+         ui.templateSize_spinBox->setMaximum(
+               m_parentWindow->m_rightImage->width());
       }
 }
 
 // begin Q_SLOTS vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-void ControlsWindow::setScanInterval(int nValue)
+void
+ControlsWindow::setScanInterval(int nValue)
 {
    m_nScanInterval = nValue;
+
+   updateParentStatusLabel();
 }
 
-void ControlsWindow::setTemplateSize(int nValue)
+void
+ControlsWindow::setTemplateSize(int nValue)
 {
    m_nTemplateSize = nValue;
+   updateParentStatusLabel();
+
 }
 // end Q_SLOTS ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+void
+ControlsWindow::updateParentStatusLabel()
+{
+   m_parentWindow->m_status_label->setText("Template Size: <b>"
+         + QString::number(m_nTemplateSize) + "x" + QString::number(
+         m_nTemplateSize) + "</b>px  |   Scan Interval: <b>" + QString::number(
+         m_nScanInterval) + "</b>px");
+}
--- src/controlswindow.h
+++ src/controlswindow.h
@@ -4,47 +4,62 @@
 #include <QtGui/QDialog>
 #include "../ui_controlswindow.h"
 
-class Qcorr;   // using this friend class
+class Qcorr; // using this friend class
 
+/** @ingroup classes */
 /** @class ControlsWindow "src/controlswindow.h"
  * @brief  allows to modify parameters for the correlation operations, such as template size and scan interval. This class is a friend of Qcorr.
  */
 class ControlsWindow : public QDialog
 {
-    Q_OBJECT
+   Q_OBJECT
 
-public:
-    ControlsWindow(Qcorr *parentWindow, QWidget *parent = 0);  ///< Constructor of ControlsWindow class must point to a Qcorr parentWindow
-    ~ControlsWindow();  ///< Closes and Destroys itself
+   public:
+      ControlsWindow(Qcorr *parentWindow, QWidget *parent = 0); ///< Constructor of ControlsWindow class must point to a Qcorr parentWindow
+      ~ControlsWindow(); ///< Closes and Destroys itself
 
-    /** @brief  Enables/Disables control spinBoxes
-      * @param bEnable boolean flag to enable or disable the spinBoxes for controlling correlation parameters of the Qcorr class
-      */
-    void setEnableSpinBoxes(bool bEnable);  ///< Set enable spinBoxes including the maximum values based on the m_parentWindow's size of its right image
+      /** @brief  Enables/Disables control spinBoxes
+       * @param bEnable boolean flag to enable or disable the spinBoxes for controlling correlation parameters of the Qcorr class
+       */
+      void
+      setEnableSpinBoxes(bool bEnable); ///< Set enable spinBoxes including the maximum values based on the m_parentWindow's size of its right image
 
-    /** @brief  to obtain the scan Interval set in the spin box
-      * @returns the scan interval value (in pixels)
-      */
-    int getScanInterval(){ return m_nScanInterval;};
+      /** @brief  to obtain the scan Interval set in the spin box
+       * @returns the scan interval value (in pixels)
+       */
+      int
+      getScanInterval()
+      {
+         return m_nScanInterval;
+      }
+      ;
 
-    /** @brief  to obtain the template's size value (as width) set in the spin box
-      * @returns the scan template's size value (in pixels)
-      */
-    int getTemplateSize(){ return m_nTemplateSize;};
+      /** @brief  to obtain the template's size value (as width) set in the spin box
+       * @returns the scan template's size value (in pixels)
+       */
+      int
+      getTemplateSize()
+      {
+         return m_nTemplateSize;
+      }
+      ;
 
-private Q_SLOTS:
-    void setScanInterval(int nValue);  ///< Q_SLOT that sets scan interval parameter in its friend Qcorr class according to the corresponding spinBox's value.
-    void setTemplateSize(int nValue);   ///< Q_SLOT that sets template size parameter in its friend Qcorr class according to the corresponding spinBox's value.
+   private Q_SLOTS:
+      void
+      setScanInterval(int nValue); ///< Q_SLOT that sets scan interval parameter in its friend Qcorr class according to the corresponding spinBox's value.
+      void
+      setTemplateSize(int nValue); ///< Q_SLOT that sets template size parameter in its friend Qcorr class according to the corresponding spinBox's value.
 
-private:
-    Ui::ControlsWindowClass ui;  ///< The Qt GUI form for this class, so its widgets can be accessed and manipulated.
-    void createActions();     ///< Create Signal-Slot connections
+   private:
+      Ui::ControlsWindowClass ui; ///< The Qt GUI form for this class, so its widgets can be accessed and manipulated.
+      void
+      createActions(); ///< Create Signal-Slot connections
+      void
+      updateParentStatusLabel(); ///< Updates Parent's status bar with the corresponding values from the control spin boxes.
 
-    Qcorr *m_parentWindow;  ///< Pointer to Main Window Object
-    int m_nScanInterval;   ///< interval of scan traversal by the template
-    int m_nTemplateSize; ///< the template's size (for the width)
-
-
+      Qcorr *m_parentWindow; ///< Pointer to Main Window Object
+      int m_nScanInterval; ///< interval of scan traversal by the template
+      int m_nTemplateSize; ///< the template's size (for the width)
 };
 
 #endif // CONTROLSWINDOW_H
